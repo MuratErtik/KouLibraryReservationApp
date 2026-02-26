5d9a19d9c52d09ee24b07f6df468d54341d21714
Murat Ertik <dmuratertik1@gmail.com>
2026-02-26 14:28:19 +0300

    first commit.

---
+++ src/main/java/org/koulibrary/koulibraryreservationapp/KouLibraryReservationAppApplication.java
@@ -0,0 +1,13 @@
+package org.koulibrary.koulibraryreservationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KouLibraryReservationAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KouLibraryReservationAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/koulibrary/koulibraryreservationapp/KouLibraryReservationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.koulibrary.koulibraryreservationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KouLibraryReservationAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
