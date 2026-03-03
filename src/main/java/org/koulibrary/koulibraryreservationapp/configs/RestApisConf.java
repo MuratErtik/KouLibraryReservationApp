ce4d1af58c16659a5bb0202617c0ead0ba00c75e
Murat Ertik <dmuratertik1@gmail.com>
2026-03-03 22:02:54 +0300

    Rest apis configuration created.

---
+++ src/main/java/org/koulibrary/koulibraryreservationapp/configs/RestApisConf.java
@@ -0,0 +1,14 @@
+package org.koulibrary.koulibraryreservationapp.configs;
+
+public class RestApisConf {
+
+    public static final String DEVELOPER = "/dev";
+    public static final String RELEASE = "/prod";
+    public static final String VERSIONS = "/v1";
+
+
+
+    public static final String LIBRARYCONTROLLER = DEVELOPER+VERSIONS+"/libraries";
+
+
+}
