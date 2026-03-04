81053a98aa0b74cabd0890f10caa72a94bac4e5b
Murat Ertik <dmuratertik1@gmail.com>
2026-03-05 01:02:00 +0300

    Library response created.

---
+++ src/main/java/org/koulibrary/koulibraryreservationapp/dtos/responses/LibraryResponse.java
@@ -0,0 +1,35 @@
+package org.koulibrary.koulibraryreservationapp.dtos.responses;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LibraryResponse {
+
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private String address;
+
+    private Integer maxActiveReservationsPerUser;
+
+    private Integer reservationWindowInDays;
+
+    private Integer checkInTimeoutMinutes; //after the reservation times how many minutes later user must check in.
+
+    private Integer checkpointIntervalMinutes;//how many minutes later user must scan the checkpoint in a row.
+
+    private Integer checkpointGraceMinutes; //after the checkpoints ready how many minutes later user must scan the checkpoint.
+
+    private Integer penaltyBlockDays;
+
+}
