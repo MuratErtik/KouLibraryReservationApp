a5e9e8632e34f4e1fbd390317c296c4503e1c9c1
Murat Ertik <dmuratertik1@gmail.com>
2026-03-04 13:59:10 +0300

    create library feature added.

--- src/main/java/org/koulibrary/koulibraryreservationapp/controllers/LibraryController.java
+++ src/main/java/org/koulibrary/koulibraryreservationapp/controllers/LibraryController.java
@@ -1,9 +1,13 @@
 package org.koulibrary.koulibraryreservationapp.controllers;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.koulibrary.koulibraryreservationapp.dtos.requests.CreateLibraryRequest;
+import org.koulibrary.koulibraryreservationapp.dtos.responses.CreateLibraryResponse;
 import org.koulibrary.koulibraryreservationapp.services.LibraryService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import static org.koulibrary.koulibraryreservationapp.configs.RestApisConf.*;
 
@@ -15,7 +19,18 @@ public class LibraryController {
     private final LibraryService libraryService;
 
     //Create
+    // security has omitted for a while @RequestHeader("Authorization") String jwt
+    //ab -n 2 -c 2 -s 600 -p createLibrary.json -T "application/json" ([...](http://localhost:8080/dev/v1/libraries))
+    // it tested and concurrency done.
+    @PostMapping
+    public ResponseEntity<CreateLibraryResponse> createLibrary(@Valid @RequestBody CreateLibraryRequest request) {
 
+        CreateLibraryResponse response = libraryService.createLibrary(request);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
+    }
 
     //Update
 
