16f1499ee722a5fb9ad53d39386bf2672e217f12
Murat Ertik <dmuratertik1@gmail.com>
2026-03-05 00:59:52 +0300

    PageResponse created.

---
+++ src/main/java/org/koulibrary/koulibraryreservationapp/dtos/responses/PageResponse.java
@@ -0,0 +1,19 @@
+package org.koulibrary.koulibraryreservationapp.dtos.responses;
+
+import lombok.*;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageResponse<T> {
+
+    private List<T> content;
+    private int pageNumber;
+    private int pageSize;
+    private long totalElements;
+    private int totalPages;
+    private boolean isLast;
+}
