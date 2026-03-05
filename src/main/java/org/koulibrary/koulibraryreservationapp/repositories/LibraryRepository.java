201702524ca5eba4ba10f387ce0909220df7fd9c
Murat Ertik <dmuratertik1@gmail.com>
2026-03-05 14:21:41 +0300

    library able to find by id.

--- src/main/java/org/koulibrary/koulibraryreservationapp/repositories/LibraryRepository.java
+++ src/main/java/org/koulibrary/koulibraryreservationapp/repositories/LibraryRepository.java
@@ -3,7 +3,13 @@ package org.koulibrary.koulibraryreservationapp.repositories;
 import org.koulibrary.koulibraryreservationapp.entities.Library;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LibraryRepository extends JpaRepository<Library, Long> {
 
     Boolean existsByName(String name);
+
+    Optional<Library> findByName(String name);
+
+    Optional<Library> findById(Long id);
 }
