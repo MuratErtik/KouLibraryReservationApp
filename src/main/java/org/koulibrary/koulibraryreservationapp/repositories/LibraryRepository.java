a1ed3d2bbb816b9d1b247caf734fa21192a1b027
Murat Ertik <dmuratertik1@gmail.com>
2026-03-05 15:01:51 +0300

    library able to find by name.

--- src/main/java/org/koulibrary/koulibraryreservationapp/repositories/LibraryRepository.java
+++ src/main/java/org/koulibrary/koulibraryreservationapp/repositories/LibraryRepository.java
@@ -1,6 +1,8 @@
 package org.koulibrary.koulibraryreservationapp.repositories;
 
 import org.koulibrary.koulibraryreservationapp.entities.Library;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -9,7 +11,7 @@ public interface LibraryRepository extends JpaRepository<Library, Long> {
 
     Boolean existsByName(String name);
 
-    Optional<Library> findByName(String name);
+    Page<Library> findByNameContainingIgnoreCase(String name, Pageable pageable);
 
     Optional<Library> findById(Long id);
 }
