b9f371374f5feb6096446be0f9096445cf85a230
Murat Ertik <dmuratertik1@gmail.com>
2026-03-05 22:16:46 +0300

    service has been reviewed.

--- src/main/java/org/koulibrary/koulibraryreservationapp/mappers/LibraryMapper.java
+++ src/main/java/org/koulibrary/koulibraryreservationapp/mappers/LibraryMapper.java
@@ -1,5 +1,6 @@
 package org.koulibrary.koulibraryreservationapp.mappers;
 
+import org.koulibrary.koulibraryreservationapp.dtos.requests.CreateLibraryRequest;
 import org.koulibrary.koulibraryreservationapp.dtos.requests.UpdateLibraryRequest;
 import org.koulibrary.koulibraryreservationapp.dtos.responses.LibraryResponse;
 import org.koulibrary.koulibraryreservationapp.entities.Library;
@@ -12,6 +13,8 @@ import org.mapstruct.NullValuePropertyMappingStrategy;
 public interface LibraryMapper {
 
     LibraryResponse toResponse(Library library);
+
+    Library toEntity(CreateLibraryRequest request);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void updateLibraryFromDto(UpdateLibraryRequest request, @MappingTarget Library library);
