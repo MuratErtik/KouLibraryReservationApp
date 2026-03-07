8c0bc2342d4c7e641491ae48052a2f307557c786
Murat Ertik <dmuratertik1@gmail.com>
2026-03-07 17:51:33 +0300

    exception created for closure.

---
+++ src/main/java/org/koulibrary/koulibraryreservationapp/exceptions/IntervalDateException.java
@@ -0,0 +1,7 @@
+package org.koulibrary.koulibraryreservationapp.exceptions;
+
+public class IntervalDateException extends RuntimeException {
+    public IntervalDateException(String message) {
+        super(message);
+    }
+}
