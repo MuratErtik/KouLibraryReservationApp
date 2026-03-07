dde5ea08e39d6f38facba73c921fff8b4504fd2c
Murat Ertik <dmuratertik1@gmail.com>
2026-03-07 14:03:16 +0300

    closure repo created.

---
+++ src/main/java/org/koulibrary/koulibraryreservationapp/repositories/LibraryClosuresRepository.java
@@ -0,0 +1,17 @@
+package org.koulibrary.koulibraryreservationapp.repositories;
+
+import org.koulibrary.koulibraryreservationapp.entities.Library;
+import org.koulibrary.koulibraryreservationapp.entities.LibraryClosures;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+
+public interface LibraryClosuresRepository extends JpaRepository<LibraryClosures, Long> {
+
+    Boolean existsByLibraryAndStartDateTimeLessThanEqualAndEndDateTimeGreaterThanEqual(
+            Library library,
+            LocalDateTime closingDate,
+            LocalDateTime openingDate
+    );
+
+}
