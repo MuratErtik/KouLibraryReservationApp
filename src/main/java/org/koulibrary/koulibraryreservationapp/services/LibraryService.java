96d411fe30cec0274fd890e0c04ce886672236e5
Murat Ertik <dmuratertik1@gmail.com>
2026-03-07 14:03:44 +0300

    create closure method implemented.

--- src/main/java/org/koulibrary/koulibraryreservationapp/services/LibraryService.java
+++ src/main/java/org/koulibrary/koulibraryreservationapp/services/LibraryService.java
@@ -2,22 +2,29 @@ package org.koulibrary.koulibraryreservationapp.services;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.koulibrary.koulibraryreservationapp.dtos.requests.CreateLibraryClosureRequest;
 import org.koulibrary.koulibraryreservationapp.dtos.requests.CreateLibraryRequest;
 import org.koulibrary.koulibraryreservationapp.dtos.requests.UpdateLibraryRequest;
+import org.koulibrary.koulibraryreservationapp.dtos.responses.CreateLibraryClosureResponse;
 import org.koulibrary.koulibraryreservationapp.dtos.responses.CreateLibraryResponse;
 import org.koulibrary.koulibraryreservationapp.dtos.responses.LibraryResponse;
 import org.koulibrary.koulibraryreservationapp.dtos.responses.PageResponse;
 import org.koulibrary.koulibraryreservationapp.entities.Library;
 
 
+import org.koulibrary.koulibraryreservationapp.entities.LibraryClosures;
+import org.koulibrary.koulibraryreservationapp.managers.LibraryClosureManager;
 import org.koulibrary.koulibraryreservationapp.managers.LibraryManager;
 
+import org.koulibrary.koulibraryreservationapp.mappers.LibraryClosuresMapper;
 import org.koulibrary.koulibraryreservationapp.mappers.LibraryMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 
+import java.time.DateTimeException;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Service
@@ -27,6 +34,10 @@ public class LibraryService {
     private final LibraryManager libraryManager;
 
     private final LibraryMapper libraryMapper;
+
+    private final LibraryClosuresMapper libraryClosuresMapper;
+
+    private final LibraryClosureManager libraryClosureManager;
 
     public CreateLibraryResponse createLibrary(@Valid CreateLibraryRequest request) {
 
@@ -99,4 +110,23 @@ public class LibraryService {
     }
 
 
+    public CreateLibraryClosureResponse createLibraryClosure(@Valid CreateLibraryClosureRequest request, Long libraryId) {
+
+        Library library = libraryManager.getLibraryById(libraryId);
+
+
+        if (request.getEndDateTime().isBefore(request.getStartDateTime())) {
+            throw new DateTimeException("Start date cannot be after end date");
+        }
+
+        LibraryClosures libraryClosures = libraryClosuresMapper.toEntity(request,library);
+
+        LibraryClosures savedLibraryClosures = libraryClosureManager.saveLibraryClosures(libraryClosures);
+
+        return CreateLibraryClosureResponse.builder()
+                .id(savedLibraryClosures.getId())
+                .message("Library closure created successfully for " + library.getName())
+                .build();
+
+    }
 }
