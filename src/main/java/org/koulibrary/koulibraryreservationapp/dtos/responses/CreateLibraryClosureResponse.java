53e6005deaac1c0db0acca4e2283cb850a22d3f9
Murat Ertik <dmuratertik1@gmail.com>
2026-03-07 14:02:20 +0300

    closure dtos have been created.

---
+++ src/main/java/org/koulibrary/koulibraryreservationapp/dtos/responses/CreateLibraryClosureResponse.java
@@ -0,0 +1,16 @@
+package org.koulibrary.koulibraryreservationapp.dtos.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateLibraryClosureResponse {
+    private String message;
+
+    private Long id;
+}
