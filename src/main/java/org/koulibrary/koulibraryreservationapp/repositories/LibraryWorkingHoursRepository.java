722ad0cfe342140e42fd905a577a351d10da85b2
Murat Ertik <dmuratertik1@gmail.com>
2026-03-08 15:32:56 +0300

    LibraryWorkingHoursRepository created.

---
+++ src/main/java/org/koulibrary/koulibraryreservationapp/repositories/LibraryWorkingHoursRepository.java
@@ -0,0 +1,12 @@
+package org.koulibrary.koulibraryreservationapp.repositories;
+
+import org.koulibrary.koulibraryreservationapp.entities.Library;
+import org.koulibrary.koulibraryreservationapp.entities.LibraryWorkingHours;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.DayOfWeek;
+
+public interface LibraryWorkingHoursRepository  extends JpaRepository<LibraryWorkingHours, Long> {
+
+    Boolean existsByDayOfWeekAndLibrary(DayOfWeek dayOfWeek, Library library);
+}
