d4c2b453ef147356030cd082dc29e4ae8688ba98
Murat Ertik <dmuratertik1@gmail.com>
2026-03-08 23:54:12 +0300

    LibraryWorkingHours dtos created.

---
+++ src/main/java/org/koulibrary/koulibraryreservationapp/dtos/responses/LibraryWorkingHoursResponse.java
@@ -0,0 +1,31 @@
+package org.koulibrary.koulibraryreservationapp.dtos.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LibraryWorkingHoursResponse {
+
+    private Long id;
+
+    private Long libraryId;
+
+    private String libraryName;
+
+    private DayOfWeek dayOfWeek;
+
+    private LocalTime openingTime;
+    private LocalTime closingTime;
+
+    // if the library closes all day or has a closure
+    private Boolean isCurrentlyOpen;
+
+}
