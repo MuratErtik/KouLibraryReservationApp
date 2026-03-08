96d5532551c74495c37cca20228c81f50bff945f
Murat Ertik <dmuratertik1@gmail.com>
2026-03-09 00:26:58 +0300

    getAll added for LibraryWorkingHours.

--- src/main/java/org/koulibrary/koulibraryreservationapp/repositories/LibraryWorkingHoursRepository.java
+++ src/main/java/org/koulibrary/koulibraryreservationapp/repositories/LibraryWorkingHoursRepository.java
@@ -2,6 +2,8 @@ package org.koulibrary.koulibraryreservationapp.repositories;
 
 import org.koulibrary.koulibraryreservationapp.entities.Library;
 import org.koulibrary.koulibraryreservationapp.entities.LibraryWorkingHours;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.DayOfWeek;
@@ -9,4 +11,6 @@ import java.time.DayOfWeek;
 public interface LibraryWorkingHoursRepository  extends JpaRepository<LibraryWorkingHours, Long> {
 
     Boolean existsByDayOfWeekAndLibrary(DayOfWeek dayOfWeek, Library library);
+
+    Page<LibraryWorkingHours> findByLibrary(Library library, Pageable pageable);
 }
