ed7459782eaae1706bf915a098bbd2da076819c4
Murat Ertik <dmuratertik1@gmail.com>
2026-03-08 15:33:34 +0300

    LibraryWorkingHoursExceptions created.

---
+++ src/main/java/org/koulibrary/koulibraryreservationapp/exceptions/InvalidWorkingHourRangeException.java
@@ -0,0 +1,7 @@
+package org.koulibrary.koulibraryreservationapp.exceptions;
+
+public class InvalidWorkingHourRangeException extends RuntimeException {
+    public InvalidWorkingHourRangeException(String message) {
+        super(message);
+    }
+}
