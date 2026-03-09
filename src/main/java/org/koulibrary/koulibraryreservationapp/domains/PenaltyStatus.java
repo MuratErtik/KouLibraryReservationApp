b65e43a24e44d06a3a5d15f1fb198ee3a789bea7
Murat Ertik <dmuratertik1@gmail.com>
2026-03-09 23:34:21 +0300

    new fields have been added.

--- src/main/java/org/koulibrary/koulibraryreservationapp/domains/PenaltyStatus.java
+++ src/main/java/org/koulibrary/koulibraryreservationapp/domains/PenaltyStatus.java
@@ -3,4 +3,6 @@ package org.koulibrary.koulibraryreservationapp.domains;
 public enum PenaltyStatus {
     ACTIVE,
     EXPIRED,
+    APPEALED,       // itiraz bekliyor
+    REVOKED         // admin tarafından kaldırıldı
 }
