0c5eb5bb518a3adb36430574f6200e583ad5709a
Murat Ertik <dmuratertik1@gmail.com>
2026-03-09 15:57:53 +0300

    saloon update feature added.

--- src/main/java/org/koulibrary/koulibraryreservationapp/mappers/SaloonMapper.java
+++ src/main/java/org/koulibrary/koulibraryreservationapp/mappers/SaloonMapper.java
@@ -2,12 +2,14 @@ package org.koulibrary.koulibraryreservationapp.mappers;
 
 import org.koulibrary.koulibraryreservationapp.domains.SaloonStatus;
 import org.koulibrary.koulibraryreservationapp.dtos.requests.CreateSaloonRequest;
+import org.koulibrary.koulibraryreservationapp.dtos.requests.UpdateLibraryClosureRequest;
+import org.koulibrary.koulibraryreservationapp.dtos.requests.UpdateSaloonRequest;
+import org.koulibrary.koulibraryreservationapp.dtos.responses.SaloonResponse;
 import org.koulibrary.koulibraryreservationapp.entities.Library;
 
 import org.koulibrary.koulibraryreservationapp.entities.LibraryClosures;
 import org.koulibrary.koulibraryreservationapp.entities.Saloon;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.*;
 
 import java.time.LocalDateTime;
 
@@ -21,5 +23,21 @@ public interface SaloonMapper {
     @Mapping(target = "tables", ignore = true)
     Saloon toEntity(CreateSaloonRequest request, Library library);
 
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    Saloon updateSaloonFromDto(UpdateSaloonRequest request,
+                                        @MappingTarget Saloon saloon);
+
+
+    @Mapping(source = "library.id", target = "libraryId")
+    @Mapping(source = "library.name", target = "libraryName")
+    //@Mapping(target = "currentDeskCount", expression = "java(calculateDeskCount(saloon))")
+    SaloonResponse toResponse(Saloon saloon);
+
+//    default Integer calculateDeskCount(Saloon saloon) {
+//        if (saloon == null || saloon.getTables() == null) {
+//            return 0;
+//        }
+//        return saloon.getTables().size();
+//    }
 
 }
