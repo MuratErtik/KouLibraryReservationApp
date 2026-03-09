1a0a206dd03948cf91bd27f31ff81616d238e995
Murat Ertik <dmuratertik1@gmail.com>
2026-03-09 23:34:00 +0300

    new fields have been added.

--- src/main/java/org/koulibrary/koulibraryreservationapp/domains/CheckpointType.java
+++ src/main/java/org/koulibrary/koulibraryreservationapp/domains/CheckpointType.java
@@ -2,5 +2,10 @@ package org.koulibrary.koulibraryreservationapp.domains;
 
 public enum CheckpointType {
     AUTO, // by system
-    MANUAL // by user
+    MANUAL, // by user
+    CHECK_IN,       // first
+    PERIODIC,       //
+    CHECK_OUT,      //
+    MISSED,         // user does not response
+    SYSTEM_EVICT    // sistem tarafından zorla sonlandırıldı
 }
