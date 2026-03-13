6853c02af4c9911c8460b90f386388e06fcc3942
Murat Ertik <dmuratertik1@gmail.com>
2026-03-13 13:11:26 +0300

    SaloonWorkingHours dtos created.

---
+++ src/main/java/org/koulibrary/koulibraryreservationapp/dtos/responses/CreateSaloonWorkingHourResponse.java
@@ -0,0 +1,17 @@
+package org.koulibrary.koulibraryreservationapp.dtos.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateSaloonWorkingHourResponse {
+
+    private String message;
+
+    private Long id;
+}
