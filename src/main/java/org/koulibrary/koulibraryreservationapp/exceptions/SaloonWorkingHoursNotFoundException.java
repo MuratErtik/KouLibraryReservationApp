9700c0e27b8c4b7c0e976b17485865479b4001d8
Murat Ertik <dmuratertik1@gmail.com>
2026-03-13 13:12:02 +0300

    SaloonWorkingHoursNotFoundException addeed.

---
+++ src/main/java/org/koulibrary/koulibraryreservationapp/exceptions/SaloonWorkingHoursNotFoundException.java
@@ -0,0 +1,7 @@
+package org.koulibrary.koulibraryreservationapp.exceptions;
+
+public class SaloonWorkingHoursNotFoundException extends RuntimeException {
+    public SaloonWorkingHoursNotFoundException(String message) {
+        super(message);
+    }
+}
