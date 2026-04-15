569f52dc960232e0b68251c48ea3b2ea20688407
Murat Ertik <dmuratertik1@gmail.com>
2026-04-15 22:18:23 +0300

    creating the desk repo created.

---
+++ src/main/java/org/koulibrary/koulibraryreservationapp/repositories/DeskRepository.java
@@ -0,0 +1,14 @@
+package org.koulibrary.koulibraryreservationapp.repositories;
+
+
+import org.koulibrary.koulibraryreservationapp.entities.Desk;
+import org.koulibrary.koulibraryreservationapp.entities.Saloon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeskRepository extends JpaRepository<Desk, Long> {
+    boolean existsByDeskNumberAndSaloon(Integer deskNumber, Saloon saloon);
+}
+
+
+
+
