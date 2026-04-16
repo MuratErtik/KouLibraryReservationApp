caedc4d259bb6ba875b10e0521e85646fcad6d08
Murat Ertik <dmuratertik1@gmail.com>
2026-04-16 15:19:20 +0300

    patch the desk dtos done

---
+++ src/main/java/org/koulibrary/koulibraryreservationapp/dtos/requests/UpdateDeskRequest.java
@@ -0,0 +1,29 @@
+package org.koulibrary.koulibraryreservationapp.dtos.requests;
+
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.*;
+import org.koulibrary.koulibraryreservationapp.domains.DeskPolicy;
+import org.koulibrary.koulibraryreservationapp.domains.DeskStatus;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UpdateDeskRequest {
+
+    @NotNull(message = "Desk number cannot be null.")
+    @Positive(message = "Desk number must be a positive value.")
+    private Integer deskNumber;
+
+    @NotNull(message = "Desk status is required.")
+    private DeskStatus status;
+
+    @NotNull(message = "Desk policy is required.")
+    private DeskPolicy policy;
+
+    @NotNull(message = "Power socket information must be specified.")
+    private Boolean hasPowerSocket;
+}
