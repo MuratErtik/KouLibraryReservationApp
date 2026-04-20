d5a4f567c74d16bb02c344076a57e5ba0d04d116
Murat Ertik <dmuratertik1@gmail.com>
2026-04-20 22:01:32 +0300

    desk uris controls done.

--- src/main/java/org/koulibrary/koulibraryreservationapp/services/DeskService.java
+++ src/main/java/org/koulibrary/koulibraryreservationapp/services/DeskService.java
@@ -2,17 +2,16 @@ package org.koulibrary.koulibraryreservationapp.services;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.jspecify.annotations.Nullable;
 import org.koulibrary.koulibraryreservationapp.dtos.requests.CreateDeskRequest;
 import org.koulibrary.koulibraryreservationapp.dtos.requests.UpdateDeskRequest;
-import org.koulibrary.koulibraryreservationapp.dtos.requests.UpdateSaloonRequest;
 import org.koulibrary.koulibraryreservationapp.dtos.responses.CreateDeskResponse;
 import org.koulibrary.koulibraryreservationapp.dtos.responses.DeskResponse;
 import org.koulibrary.koulibraryreservationapp.dtos.responses.PageResponse;
-import org.koulibrary.koulibraryreservationapp.dtos.responses.SaloonResponse;
 import org.koulibrary.koulibraryreservationapp.entities.Desk;
 import org.koulibrary.koulibraryreservationapp.entities.Library;
 import org.koulibrary.koulibraryreservationapp.entities.Saloon;
+import org.koulibrary.koulibraryreservationapp.exceptions.DeskDoesNotBelongToSaloonException;
+import org.koulibrary.koulibraryreservationapp.exceptions.SaloonDoesNotBelongToLibraryException;
 import org.koulibrary.koulibraryreservationapp.managers.DeskManager;
 import org.koulibrary.koulibraryreservationapp.managers.LibraryManager;
 import org.koulibrary.koulibraryreservationapp.managers.SaloonManager;
@@ -42,6 +41,10 @@ public class DeskService {
 
         Saloon saloon = saloonManager.getSaloonById(saloonId);
 
+        if (!saloon.getLibrary().getId().equals(library.getId())) {
+            throw new SaloonDoesNotBelongToLibraryException("Saloon with id "+saloonId+" doesn't belong to library with id: " + libraryId);
+        }
+
         Desk desk = deskMapper.toEntity(request,saloon);
 
         Desk savedDesk = deskManager.saveDesk(desk);
@@ -61,7 +64,16 @@ public class DeskService {
 
         Saloon saloon = saloonManager.getSaloonById(saloonId);
 
+        if (!saloon.getLibrary().getId().equals(library.getId())) {
+            throw new SaloonDoesNotBelongToLibraryException("Saloon with id "+saloonId+" doesn't belong to library with id: " + libraryId);
+        }
+
+
         Desk desk = deskManager.getDeskById(deskId);
+
+        if(!desk.getSaloon().getId().equals(saloon.getId())){
+            throw new DeskDoesNotBelongToSaloonException("The Desk does not belong to the saloon with id: " + saloonId);
+        }
 
         // number+saloon
         deskManager.checkNameConflict(desk, request.getDeskNumber());
@@ -80,7 +92,16 @@ public class DeskService {
 
         Saloon saloon = saloonManager.getSaloonById(saloonId);
 
+        if (!saloon.getLibrary().getId().equals(library.getId())) {
+            throw new SaloonDoesNotBelongToLibraryException("Saloon with id "+saloonId+" doesn't belong to library with id: " + libraryId);
+        }
+
+
         Desk desk = deskManager.getDeskById(deskId);
+
+        if(!desk.getSaloon().getId().equals(saloon.getId())){
+            throw new DeskDoesNotBelongToSaloonException("The Desk does not belong to the saloon with id: " + saloonId);
+        }
 
         return deskMapper.toResponse(desk);
 
@@ -120,7 +141,16 @@ public class DeskService {
 
         Saloon saloon = saloonManager.getSaloonById(saloonId);
 
+        if (!saloon.getLibrary().getId().equals(library.getId())) {
+            throw new SaloonDoesNotBelongToLibraryException("Saloon with id "+saloonId+" doesn't belong to library with id: " + libraryId);
+        }
+
+
         Desk desk = deskManager.getDeskById(deskId);
+
+        if(!desk.getSaloon().getId().equals(saloon.getId())){
+            throw new DeskDoesNotBelongToSaloonException("The Desk does not belong to the saloon with id: " + saloonId);
+        }
 
         deskManager.deleteDeskById(deskId);
     }
