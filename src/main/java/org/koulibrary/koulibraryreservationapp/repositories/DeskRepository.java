0b872a465e3c8bafef4c80744a9fc95c592e45e1
Murat Ertik <dmuratertik1@gmail.com>
2026-04-21 16:48:01 +0300

    JpaSpecificationExecutor added.

--- src/main/java/org/koulibrary/koulibraryreservationapp/repositories/DeskRepository.java
+++ src/main/java/org/koulibrary/koulibraryreservationapp/repositories/DeskRepository.java
@@ -6,10 +6,11 @@ import org.koulibrary.koulibraryreservationapp.entities.Saloon;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Set;
 
-public interface DeskRepository extends JpaRepository<Desk, Long> {
+public interface DeskRepository extends JpaRepository<Desk, Long> , JpaSpecificationExecutor<Desk> {
     boolean existsByDeskNumberAndSaloon(Integer deskNumber, Saloon saloon);
 
     boolean existsByDeskNumber(Integer deskNumber);
