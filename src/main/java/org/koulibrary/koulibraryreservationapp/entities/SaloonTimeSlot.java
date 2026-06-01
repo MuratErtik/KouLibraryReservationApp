5c7646349cbe55d008ec0bfcb23b6225288d2f9b
Murat Ertik <dmuratertik1@gmail.com>
2026-06-01 11:49:10 +0300

    renamed from lib to saloon.

--- src/main/java/org/koulibrary/koulibraryreservationapp/entities/SaloonTimeSlot.java
+++ src/main/java/org/koulibrary/koulibraryreservationapp/entities/SaloonTimeSlot.java
@@ -1,33 +1,40 @@
 package org.koulibrary.koulibraryreservationapp.entities;
 import jakarta.persistence.*;
 import lombok.*;
-import java.time.DayOfWeek;
+
+import java.time.LocalDate;
 import java.time.LocalTime;
 
 
 @Entity
 @Table(
-        name = "library_time_slots",
-        uniqueConstraints = @UniqueConstraint(columnNames = {"library_id", "day_of_week", "start_time"})
+        name = "saloon_time_slots",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"saloon_id", "date", "start_time"})
 )
-@Getter @Setter
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class LibraryTimeSlot {
+public class SaloonTimeSlot {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "library_id", nullable = false)
-    private Library library;
+    @JoinColumn(name = "saloon_id", nullable = false)
+    private Saloon saloon;
 
 
-    @Enumerated(EnumType.STRING)
-    @Column(name = "day_of_week", nullable = false)
-    private DayOfWeek dayOfWeek;
+//    @Enumerated(EnumType.STRING)
+//    @Column(name = "day_of_week", nullable = false)
+//    private DayOfWeek dayOfWeek;
+
+
+    //instead of day of week
+    @Column(nullable = false)
+    private LocalDate date;
 
     @Column(nullable = false)
     private LocalTime startTime;
