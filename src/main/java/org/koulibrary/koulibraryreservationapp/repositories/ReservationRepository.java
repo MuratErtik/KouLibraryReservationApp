9e4c6000b07d034abf38d9baa61c57745ec2a9c7
Murat Ertik <dmuratertik1@gmail.com>
2026-06-06 20:35:10 +0300

    ReservationRepository created.

---
+++ src/main/java/org/koulibrary/koulibraryreservationapp/repositories/ReservationRepository.java
@@ -0,0 +1,19 @@
+package org.koulibrary.koulibraryreservationapp.repositories;
+
+import org.koulibrary.koulibraryreservationapp.domains.ReservationStatus;
+import org.koulibrary.koulibraryreservationapp.entities.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    @Query("SELECT r FROM Reservation r WHERE r.slot.id = :slotId AND r.status IN :statuses")
+    List<Reservation> findBySlotIdAndStatusIn(@Param("slotId") Long slotId,
+                                              @Param("statuses") Collection<ReservationStatus> statuses);
+
+    
+}
