688d3cd9fbf3dc6c27ea86a56d9acc807211a135
Murat Ertik <dmuratertik1@gmail.com>
2026-06-08 18:38:42 +0300

    auth dtos added.

---
+++ src/main/java/org/koulibrary/koulibraryreservationapp/dtos/responses/TokenResponse.java
@@ -0,0 +1,29 @@
+package org.koulibrary.koulibraryreservationapp.dtos.responses;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenResponse {
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("expires_in")
+    private long expiresIn;
+
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+
+    @JsonProperty("refresh_expires_in")
+    private long refreshExpiresIn;
+
+    @JsonProperty("token_type")
+    private String tokenType;
+}
