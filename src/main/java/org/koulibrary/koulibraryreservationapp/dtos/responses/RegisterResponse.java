e3ee00e7b0710e4d873cc7e03933a37d24f4f29c
Murat Ertik <dmuratertik1@gmail.com>
2026-06-08 13:05:33 +0300

    register dtos created.

---
+++ src/main/java/org/koulibrary/koulibraryreservationapp/dtos/responses/RegisterResponse.java
@@ -0,0 +1,17 @@
+package org.koulibrary.koulibraryreservationapp.dtos.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterResponse {
+
+    Long id;
+    String studentIdNumber;
+    String message;
+}
