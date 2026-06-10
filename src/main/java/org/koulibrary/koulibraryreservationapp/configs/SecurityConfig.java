3eaf3f5e9cf4917b99a043096120907079bc5b9f
Murat Ertik <dmuratertik1@gmail.com>
2026-06-10 17:07:09 +0300

    swagger added into requestmatchers.

--- src/main/java/org/koulibrary/koulibraryreservationapp/configs/SecurityConfig.java
+++ src/main/java/org/koulibrary/koulibraryreservationapp/configs/SecurityConfig.java
@@ -51,6 +51,12 @@ public class SecurityConfig {
 
                         .requestMatchers(HttpMethod.GET,    LIBRARYCONTROLLER + "/**").authenticated()
 
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**",
+                                "/v3/api-docs.yaml"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
