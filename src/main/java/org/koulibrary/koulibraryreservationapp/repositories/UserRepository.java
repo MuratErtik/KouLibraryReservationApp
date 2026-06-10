30571ce017f7ff77f2dddbd9bf7997bc626c9ea7
Murat Ertik <dmuratertik1@gmail.com>
2026-06-11 00:32:00 +0300

    findByKeycloakIdForUpdate added for consistency

--- src/main/java/org/koulibrary/koulibraryreservationapp/repositories/UserRepository.java
+++ src/main/java/org/koulibrary/koulibraryreservationapp/repositories/UserRepository.java
@@ -1,7 +1,12 @@
 package org.koulibrary.koulibraryreservationapp.repositories;
 
+import jakarta.persistence.LockModeType;
 import org.koulibrary.koulibraryreservationapp.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 
 import java.util.Optional;
 
@@ -14,4 +19,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Boolean existsByStudentIdNumber(String studentIdNumber);
 
     Boolean existsByEmail(String email);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT u FROM User u WHERE u.keycloakId = :keycloakId")
+    Optional<User> findByKeycloakIdForUpdate(@Param("keycloakId") String keycloakId);
 }
