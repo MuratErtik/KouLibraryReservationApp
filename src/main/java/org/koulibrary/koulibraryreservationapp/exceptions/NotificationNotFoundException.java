86d7177b9d6fc40e1526cc75a51b478037ad29bf
Murat Ertik <dmuratertik1@gmail.com>
2026-06-13 18:17:09 +0300

    Notification exceptions added.

---
+++ src/main/java/org/koulibrary/koulibraryreservationapp/exceptions/NotificationNotFoundException.java
@@ -0,0 +1,7 @@
+package org.koulibrary.koulibraryreservationapp.exceptions;
+
+public class NotificationNotFoundException extends RuntimeException {
+    public NotificationNotFoundException(String message) {
+        super(message);
+    }
+}
